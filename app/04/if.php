8dd81e3b24e7f30e8ceade45d58e66c30c18a514
乗田茉以子 <[maiko98n@hotmail.co.jp]>
2019-10-14 18:32:47 +0900

    04-02

--- app/04/if.php
+++ app/04/if.php
@@ -3,11 +3,11 @@ $langauge = 2;
 
 if($language === 1){
     echo 'こんにちは';
-}elseif( $language = 2){
+}elseif( $language === 2){
     echo 'hello';
-}elseif($language = 3){
+}elseif($language === 3){
     echo 'bonjour';
 }else{
-    echo '入力するした数値が違います';
+    echo '入力した数値が違います';
 }
 ?>
